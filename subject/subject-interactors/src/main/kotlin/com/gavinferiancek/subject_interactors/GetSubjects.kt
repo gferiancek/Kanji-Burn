9a9e0a170a389b6b34c8747c7bfb2393fce56274
gferiancek <gferiancek@gmail.com>
2022-02-04 22:25:07 -0500

    Changed network call to run on IO thread.  Added note on kotlin's async/awaitAll for future use when implementing the initial startup page that loads data from all of the endpoints.

--- subject/subject-interactors/src/main/kotlin/com/gavinferiancek/subject_interactors/GetSubjects.kt
+++ subject/subject-interactors/src/main/kotlin/com/gavinferiancek/subject_interactors/GetSubjects.kt
@@ -9,11 +9,10 @@ import com.gavinferiancek.subject_domain.Kanji
 import com.gavinferiancek.subject_domain.Radical
 import com.gavinferiancek.subject_domain.Subject
 import com.gavinferiancek.subject_domain.Vocab
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.withContext
 
 class GetSubjects(
     val service: SubjectService,
@@ -24,15 +23,21 @@ class GetSubjects(
 
             val apiKey = "1b74da00-b68f-4a4a-8e96-b6638d706013"
             val subjects = mutableListOf<Subject>()
-            var response = service.getSubjects(apiKey, null)
             var page = 1
-            var nextUrl = response.pageData.nextUrl
-            subjects.addAll(response.data.toSubjectList())
-            while (nextUrl != null) {
-                response = service.getSubjects(apiKey, page * 1000)
-                nextUrl = response.pageData.nextUrl
-                page += 1
+
+            withContext(Dispatchers.IO) {
+                //When it comes to syncing up all of the data on the first load, use a list of kotlin async tasks.
+                // You can awaitAll() to ensure all data is loaded before proceeding to the dashboard.
+                // Example: https://www.baeldung.com/kotlin/coroutines-waiting-for-multiple-threads
+                var response = service.getSubjects(apiKey, null)
+                var nextUrl = response.pageData.nextUrl
                 subjects.addAll(response.data.toSubjectList())
+                while (nextUrl != null) {
+                    response = service.getSubjects(apiKey, page * 1000)
+                    nextUrl = response.pageData.nextUrl
+                    page += 1
+                    subjects.addAll(response.data.toSubjectList())
+                }
             }
             // SubjectListScreen uses a ViewPager to show separate list for each type of subject.
             // We return a list of lists so that we can sync that to the TabRow. (1st tab = first list, etc)
@@ -42,13 +47,16 @@ class GetSubjects(
                 subjects.filterIsInstance<Vocab>(),
             )
             emit(DataState.Data(data = instancedSubjects))
-        } catch(e: Exception) {
-            emit(DataState.Response(
-                uiComponent = UIComponent.None(
-                    message = e.message?: "Unknown error",
-                )))
-        }
-        finally {
+
+        } catch (e: Exception) {
+            emit(
+                DataState.Response(
+                    uiComponent = UIComponent.None(
+                        message = e.message ?: "Unknown error",
+                    )
+                )
+            )
+        } finally {
             emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
         }
     }
